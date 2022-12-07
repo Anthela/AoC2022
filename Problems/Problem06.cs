3e99374e37ee05d3e508a91a1c19e7d32adbfd17
Anthela <nwwyf7@gmail.com>
2022-12-07 21:34:59 +0100

    Day 6

---
+++ Problems/Problem06.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_22.Problems
+{
+    public class Problem06 : IProblem<int, int>
+    {
+        public int DoPartA()
+        {
+            return CharacterProcessed(4);
+        }
+
+        public int DoPartB()
+        {
+            return CharacterProcessed(14);
+        }
+
+        private int CharacterProcessed(int distinctChars)
+        {
+            var line = Utils.InputToStringArray("06").Single();
+
+            var que = new Queue<char>();
+            var i = 0;
+
+            foreach (var item in line)
+            {
+                que.Enqueue(item);
+                i++;
+
+                if (que.Count() == distinctChars)
+                {
+                    if (que.Distinct().Count() == distinctChars)
+                        return i;
+
+                    que.Dequeue();
+                }
+            }
+
+            return 0;
+        }
+    }
+}
